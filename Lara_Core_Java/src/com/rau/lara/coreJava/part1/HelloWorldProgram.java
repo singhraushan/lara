57018e1ac45fa4c4a4490e6d72eee522fc0b03fa
raushan <raushan@raushan-Vostro-15-3568>
2018-06-09 19:22:12 +0530

    1st commit

---
+++ Lara_Core_Java/src/com/rau/lara/coreJava/part1/HelloWorldProgram.java
@@ -0,0 +1,10 @@
+package com.rau.lara.coreJava.part1;
+
+public class HelloWorldProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
